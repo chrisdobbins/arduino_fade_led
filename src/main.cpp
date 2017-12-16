9b21e28c6ab6b0564ffdbad89d085317dbbd86e5
christopher dobbins <christopherjosephdobbins@gmail.com>
2017-12-15 22:56:07 -0500

    checking in WIP using interrupts to toggle modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,65 @@
 #include <Arduino.h>
 #include <math.h>
 
-const int DELAY = 1000;
+int DELAY = 2000;
+
 const byte RED_PIN = 9;
-const byte GRN_PIN = 6;
-const byte BLU_PIN = 5;
+const byte GRN_PIN = 10;
+const byte BLU_PIN = 11;
+
+const byte INTERRUPT_PIN = 2;
+
 unsigned long time;
+
+volatile int counter = 0;
+volatile bool RESET_PINS = false;
 
 void blend(byte pin1, byte pin2);
 float calculateLightValueOne(unsigned long time);
 float calculateLightValueTwo(unsigned long time);
 
+void flash();
+void fade();
+void resetLEDPins();
+void interruptHandler();
+
 void setup() {
-  // Serial.begin(9600);
+    Serial.begin(9600);
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interruptHandler, RISING);
+}
+void loop() {
+   if (RESET_PINS){
+       resetLEDPins();
+       RESET_PINS = false;
+   }
+
+   if (counter%2 == 0) {
+       fade();
+   } else {
+       flash();
+   }
 }
 
-void loop() {
-  // blend works by using reciprocal sine functions
-  // the colors are blended as follows:
-  // one is increased from 0, the other starts at 1 * 255 and decreases
-  // when it is time to change colors, to avoid the appearance of a
-  // muddy white color, the value that decreased from 255 to 0 is
-  // replaced with the color to be added in.
-  // the periodicity(?) is 10 s, so the delay should be multiples of 10s
+void resetLEDPins() {
+  analogWrite(BLU_PIN, 0);
+  analogWrite(RED_PIN, 0);
+  analogWrite(GRN_PIN, 0);
+}
 
+void interruptHandler() {
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+
+    if (interrupt_time - last_interrupt_time > DELAY) {
+        counter++;
+        RESET_PINS = true;
+    }
+    last_interrupt_time = interrupt_time;
+}
+
+
+void fade() {
   // start with red --> green
   blend(RED_PIN, GRN_PIN);
   // add blue, take away green
@@ -36,14 +72,33 @@ void loop() {
   blend(GRN_PIN, BLU_PIN);
   // add red, take away blue
   blend(RED_PIN, BLU_PIN);
+}
 
-  // NB: this is done with a series of 6 steps to allow
-  // the colors to 'wrap around'. there are 3 combinations
-  // but every run of blend() alternates which values are
-  // high (~255) or low (~0). to ensure that the alternating
-  // end states of the params don't interfere with the color
-  // transitions, 6 transitions are done to get back to
-  // the starting color (red)
+void flash() {
+    Serial.println("inside flash: ");
+    Serial.println(DELAY);
+    float colors[8][3] = {{1, 0, 0}, // RED
+                         {1, .25, 0}, // ORANGE
+                         {1, 1, 0}, // YELLOW
+                         {0, 1, 0}, // GREEN
+                         {0, 0, 1}, // BLUE
+                         {.25, 0, 1}, // VIOLET
+                         {0, 1, 1}, // CYAN
+                         {1, 0, 1} // PINK
+                         };
+    unsigned long previousTime = 0;
+    for (int i=0; i<8; i++) {
+        unsigned long start = millis();
+        unsigned long currentTime = start;
+       do {
+            analogWrite(RED_PIN, static_cast<byte>(255*colors[i][0]));
+            analogWrite(GRN_PIN, static_cast<byte>(255*colors[i][1]));
+            analogWrite(BLU_PIN, static_cast<byte>(255*colors[i][2]));
+            currentTime = millis();
+
+        } while((currentTime - start <= DELAY/2));
+    }
+
 }
 
 float calculateLightValueOne(unsigned long time) {
@@ -54,7 +109,24 @@ float calculateLightValueTwo(unsigned long time) {
   return (.5 * sin((time/(DELAY * 1.0) * PI) - (PI * .5))) + .5;
 }
 
+  // blend works by using reciprocal sine functions
+  // the colors are blended as follows:
+  // one is increased from 0, the other starts at 1 * 255 and decreases
+  // when it is time to change colors, to avoid the appearance of a
+  // muddy white color, the value that decreased from 255 to 0 is
+  // replaced with the color to be added in.
+
+  // changes are done with a series of 6 steps to allow
+  // the colors to 'wrap around'. there are 3 combinations
+  // but every run of blend() alternates which values are
+  // high (~255) or low (~0). to ensure that the alternating
+  // end states of the params don't interfere with the color
+  // transitions, 6 transitions are done to get back to
+  // the starting color (red)
+
 void blend(byte pin1, byte pin2) {
+    Serial.println("inside blend: ");
+    Serial.println(DELAY);
   unsigned long start = millis();
   unsigned long time = start;
 
