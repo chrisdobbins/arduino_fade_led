840a02666de671a625e9d00ba2fe596fb69c5a8f
christopher dobbins <christopherjosephdobbins@gmail.com>
2018-01-08 01:08:14 -0500

    adding modified fade/blend fns for use with PCA9685 driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
+#include <Wire.h>
+#include <PCA9685.h>
 #include <Arduino.h>
 #include <math.h>
-
+// PCA9685 with default settings
+PCA9685 driver = PCA9685(0x40, PCA9685_MODE_N_DRIVER, PCA9685_MAX_FREQUENCY);
 int DELAY = 20000;
-
+/*
 const byte RED_PIN = 9;
 const byte GRN_PIN = 10;
 const byte BLU_PIN = 11;
-
+*/
 unsigned long time;
 
 void fade();
@@ -14,62 +17,81 @@ void blend(byte pin1, byte pin2);
 float calculateLightValueOne(unsigned long time);
 float calculateLightValueTwo(unsigned long time);
 
+void setup()
+{
+  // initialize serial for debug output
+  Serial.begin(9600);
 
-void setup() {
+  // initialize TwoWire communication
+  Wire.begin();
+
+  // setup the PCA9685 
+  driver.setup();
 }
 
-void loop() {
-    fade();
+void loop()
+{
+  // setting up pin to drive common + and GND rails for 
+  // transistors, 100 ohm resistor and capacitor
+  driver.getPin(PCA9685_LED15).fullOnAndWrite();
+
+  fade();
+/*
+  // go through all possible 512 of 4096 possible pwm steps in the first LED
+  Serial.println("FADE IN LED0");
+  int i;
+  for (i = 0; i < PCA9685_MAX_VALUE; i = i + 8)
+  {
+    // set the pwm value of the first led
+    driver.getPin(PCA9685_LED0).setValueAndWrite(i);
+    delay(5);
+  }
+  Serial.println("FADE OUT LED0");
+  for (i = PCA9685_MAX_VALUE; i >= 0; i = i - 8)
+  {
+    // set the pwm value of the first led
+    driver.getPin(PCA9685_LED0).setValueAndWrite(i);
+    delay(5);
+  }*/
 }
 
 void fade() {
-  // start with red --> green
-  blend(RED_PIN, GRN_PIN);
-  // add blue, take away green
-  blend(BLU_PIN, GRN_PIN);
-  // add red, take away blue
-  blend(BLU_PIN, RED_PIN);
-  // add green, take away red
-  blend(GRN_PIN, RED_PIN);
-  // add blue, take away green
-  blend(GRN_PIN, BLU_PIN);
-  // add red, take away blue
-  blend(RED_PIN, BLU_PIN);
+    const int RED_PIN = PCA9685_LED0;
+    const int GRN_PIN = PCA9685_LED1;
+    const int BLU_PIN = PCA9685_LED2;
+
+      // start with red --> green
+    blend(RED_PIN, GRN_PIN);
+    // add blue, take away green
+    blend(BLU_PIN, GRN_PIN);
+    // add red, take away blue
+    blend(BLU_PIN, RED_PIN);
+    // add green, take away red
+    blend(GRN_PIN, RED_PIN);
+    // add blue, take away green
+    blend(GRN_PIN, BLU_PIN);
+    // add red, take away blue
+    blend(RED_PIN, BLU_PIN);
 }
 
-  // blend works by using reciprocal sine functions
-  // the colors are blended as follows:
-  // one is increased from 0, the other starts at 1 * 255 and decreases
-  // when it is time to change colors, to avoid the appearance of a
-  // muddy white color, the value that decreased from 255 to 0 is
-  // replaced with the color to be added in.
-
-  // changes are done with a series of 6 steps to allow
-  // the colors to 'wrap around'. there are 3 combinations
-  // but every run of blend() alternates which values are
-  // high (~255) or low (~0). to ensure that the alternating
-  // end states of the params don't interfere with the color
-  // transitions, 6 transitions are done to get back to
-  // the starting color (red)
-
 void blend(byte pin1, byte pin2) {
-  unsigned long start = millis();
-  unsigned long time = start;
+     unsigned long start = millis();
+     unsigned long time = start;
 
-  while (time < start + DELAY) {
-    byte pinSignal1 = static_cast<byte>(255 * calculateLightValueOne(time));
-    analogWrite(pin1, pinSignal1);
-    byte pinSignal2 = static_cast<byte>(255 * calculateLightValueTwo(time));
-    analogWrite(pin2, pinSignal2);
-    time = millis();
-  }
+     while (time < start + DELAY) {
+          int pinSignal1 = static_cast<int>(4095 * calculateLightValueOne(time));
+          driver.getPin(pin1).setValueAndWrite(pinSignal1);
+          int pinSignal2 = static_cast<int>(4095 * calculateLightValueTwo(time));
+          driver.getPin(pin2).setValueAndWrite(pinSignal2);
+          time = millis();
+     }
 }
 
 float calculateLightValueOne(unsigned long time) {
-  return (-.5 * sin((time/(DELAY * 1.0) * PI) - (PI * .5))) + .5;
+      return (-.5 * sin((time/(DELAY * 1.0) * PI) - (PI * .5))) + .5;
 }
 
 float calculateLightValueTwo(unsigned long time) {
-  return (.5 * sin((time/(DELAY * 1.0) * PI) - (PI * .5))) + .5;
+      return (.5 * sin((time/(DELAY * 1.0) * PI) - (PI * .5))) + .5;
 }
 
